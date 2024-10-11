8750ac282a8c2dd00c75aa0f8c334f51811357fa
Incept1on <incept1onnn999@gmail.com>
2024-10-11 14:23:41 +0300

    remanage directory

--- main.py
+++ main.py
@@ -1,42 +1,8 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
 from typing import List
 
 app = FastAPI()
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <input type="text" id="messageText" autocomplete="off"/>
-        <button onclick="sendMessage()">Send</button>
-        <ul id='messages'>
-        </ul>
-        <script>
-            const clientId = prompt("Enter your client ID:");
-            const ws = new WebSocket(`ws://localhost:8000/ws/${clientId}`);
-            
-            ws.onmessage = function(event) {
-                const messages = document.getElementById('messages');
-                const message = document.createElement('li');
-                const content = document.createTextNode(event.data);
-                message.appendChild(content);
-                messages.appendChild(message);
-            };
-
-            function sendMessage() {
-                const input = document.getElementById("messageText");
-                ws.send(input.value);
-                input.value = '';
-            }
-        </script>
-    </body>
-</html>
-"""
 
 class ConnectionManager:
     def __init__(self):
@@ -63,7 +29,7 @@ manager = ConnectionManager()
 
 @app.get("/")
 async def get():
-    return HTMLResponse(html)
+    return FileResponse("static/chat.html")
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: str):
